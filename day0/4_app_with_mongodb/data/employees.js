4c8c14819056806b0edadff445fd708cebfe2fd5
aaami1ster <aaami1ster@gmail.com>
2018-05-30 14:51:27 +0300

    update

---
+++ day0/4_app_with_mongodb/data/employees.js
@@ -0,0 +1,82 @@
+[
+    { 
+        "_id" : ObjectId("5aa808f466d5ec2ff6d16090"),
+        "name" : "abdalla", 
+        "departmentId": ObjectId("5aa7c707b88a9d1fdae0ed50"),
+        "updatedAt": ISODate("2018-03-14T07:47:36.908+0000"),
+        "createdAt": ISODate("2018-03-14T07:04:03.130+0000"),
+        "__v" : NumberInt(0)
+    },
+    { 
+        "_id" : ObjectId("5aa808f466d5ec2ff6d16091"),
+        "name" : "omer", 
+        "departmentId": ObjectId("5aa7c707b88a9d1fdae0ed50"),
+        "updatedAt": ISODate("2018-03-14T07:47:36.908+0000"),
+        "createdAt": ISODate("2018-03-14T07:04:03.130+0000"),
+        "__v" : NumberInt(0)
+    },
+    { 
+        "_id" : ObjectId("5aa808f466d5ec2ff6d16092"),
+        "name" : "hassan", 
+        "departmentId": ObjectId("5aa7c707b88a9d1fdae0ed50"),
+        "updatedAt": ISODate("2018-03-14T07:47:36.908+0000"),
+        "createdAt": ISODate("2018-03-14T07:04:03.130+0000"),
+        "__v" : NumberInt(0)
+    },
+    { 
+        "_id" : ObjectId("5aa808f466d5ec2ff6d16093"),
+        "name" : "mohammed", 
+        "departmentId": ObjectId("5aa7c707b88a9d1fdae0ed51"),
+        "updatedAt": ISODate("2018-03-14T07:47:36.908+0000"),
+        "createdAt": ISODate("2018-03-14T07:04:03.130+0000"),
+        "__v" : NumberInt(0)
+    },
+    { 
+        "_id" : ObjectId("5aa808f466d5ec2ff6d16094"),
+        "name" : "ahmed", 
+        "departmentId": ObjectId("5aa7c707b88a9d1fdae0ed51"),
+        "updatedAt": ISODate("2018-03-14T07:47:36.908+0000"),
+        "createdAt": ISODate("2018-03-14T07:04:03.130+0000"),
+        "__v" : NumberInt(0)
+    },
+    { 
+        "_id" : ObjectId("5aa808f466d5ec2ff6d16095"),
+        "name" : "walleed", 
+        "departmentId": ObjectId("5aa7c707b88a9d1fdae0ed51"),
+        "updatedAt": ISODate("2018-03-14T07:47:36.908+0000"),
+        "createdAt": ISODate("2018-03-14T07:04:03.130+0000"),
+        "__v" : NumberInt(0)
+    },
+    { 
+        "_id" : ObjectId("5aa808f466d5ec2ff6d16096"),
+        "name" : "abbas", 
+        "departmentId": ObjectId("5aa7c707b88a9d1fdae0ed52"),
+        "updatedAt": ISODate("2018-03-14T07:47:36.908+0000"),
+        "createdAt": ISODate("2018-03-14T07:04:03.130+0000"),
+        "__v" : NumberInt(0)
+    },
+    { 
+        "_id" : ObjectId("5aa808f466d5ec2ff6d16097"),
+        "name" : "mohaned", 
+        "departmentId": ObjectId("5aa7c707b88a9d1fdae0ed52"),
+        "updatedAt": ISODate("2018-03-14T07:47:36.908+0000"),
+        "createdAt": ISODate("2018-03-14T07:04:03.130+0000"),
+        "__v" : NumberInt(0)
+    },
+    { 
+        "_id" : ObjectId("5aa808f466d5ec2ff6d16098"),
+        "name" : "osman", 
+        "departmentId": ObjectId("5aa7c707b88a9d1fdae0ed52"),
+        "updatedAt": ISODate("2018-03-14T07:47:36.908+0000"),
+        "createdAt": ISODate("2018-03-14T07:04:03.130+0000"),
+        "__v" : NumberInt(0)
+    },
+    { 
+        "_id" : ObjectId("5aa808f466d5ec2ff6d16099"),
+        "name" : "khalid", 
+        "departmentId": ObjectId("5aa7c707b88a9d1fdae0ed53"),
+        "updatedAt": ISODate("2018-03-14T07:47:36.908+0000"),
+        "createdAt": ISODate("2018-03-14T07:04:03.130+0000"),
+        "__v" : NumberInt(0)
+    }
+]
