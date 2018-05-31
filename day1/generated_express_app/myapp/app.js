6f823ded915421530e26ff23b7b1141216add180
aaami1ster <aaami1ster@gmail.com>
2018-05-31 12:46:21 +0300

    add express app

---
+++ day1/generated_express_app/myapp/app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+/* ########################## connect to database #####################################*/
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mydb');
+
+// Close the Mongoose connection on Control+C
+process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected');
+        process.exit(0);
+    });
+});
+require('./models/employee');
+require('./models/department');
+
+
+
+var employees = require('./routes/employees');
+var departments = require('./routes/departments');
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// application routes
+app.use(employees);
+app.use(departments);
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ day1/generated_express_app/myapp/routes/employees.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var Employee = mongoose.model('Employee');
+var department = mongoose.model('Department');
+var router = express.Router();
+router.get('/employees', function(req, res, next) {
+  Employee.find().sort('name.last').exec(function(error, results) {
+    if (error) {
+      return next(error);
+}
+    // Respond with valid data
+    res.json(results);
+  });
+});
+router.get('/employees/:employeeId', function(req, res, next) {
+  Employee.findOne({
+    _id: req.params.employeeId
+  }).populate('department').exec(function (error, results) {
+    if (error) {
+      return next(error);
+}
+    // If valid user was not found, send 404
+    if (!results) {
+      return res.sendStatus(404);
+    }
+    // Respond with valid data
+    return res.json(results);
+  });
+});
+router.put('/employees/:employeeId', function (req, res, next) {
+  // Remove this or mongoose will throw an error
+  // because we would be trying to update the mongo ID
+  delete req.body._id;
+  req.body.department = req.body.department._id;
+  Employee.update({
+    id: req.params.employeeId
+  }, req.body, function (err, numberAffected, response) {
+    if (err) {
+      return next(err);
+    }
+    return res.sendStatus(200);
+  });
+});
+module.exports = router;
